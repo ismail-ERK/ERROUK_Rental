7b5e890078ba2896f836181ccb5baa24754b0eba
ismail <ismailerrouk9@gmail.com>
2022-03-02 10:38:44 +0100

    [ismail : add cors ]

---
+++ src/main/java/com/example/location_voitures/controllers/PaymentsController.java
@@ -0,0 +1,10 @@
+package com.example.location_voitures.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class PaymentsController {
+
+}
